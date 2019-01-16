3ad16f072105e93a618f415dd296f450d1a193a7
LaoBing_Fung <FLB_B401@163.com>
2019-01-17 02:26:11 +0800

    load comments from douban api

--- js/comments.js
+++ js/comments.js
@@ -4,7 +4,7 @@ class Comments {
     this.movieId = movieId;
   }
   setRequest() {
-    this.request.url = `https://api.douban.com/v2/movie/subject/${this.movieId}/comments?apikey=0b2bdeda43b5688921839c8ecb20399b&start=20&count=20&client=&udid=`;
+    this.request.url = `https://api.douban.com/v2/movie/subject/${this.movieId}/comments?apikey=0b2bdeda43b5688921839c8ecb20399b&start=0&count=20&client=&udid=`;
     this.request.dataType = 'jsonp';
     this.request.success = this.callback.bind(this);
   }
@@ -19,6 +19,9 @@ class Comments {
     setDetail(this.subject.pubdates, 'pubdates');
     setDetail(this.subject.durations, 'durations');
     setRating(this.subject.rating.average);
+    for (let i = 0; i < 5; i++) {
+      createOneComment(this.comments[i]);
+    }
   }
   getComments() {
     $.ajax(this.request);
@@ -31,17 +34,24 @@ function createOneComment(comment) {
   let time = comment.created_at;
   let content = comment.content;
   let html = `<div class="comment_item">
-          <div class="comment_head">
-            ${commentator}
-            <span class="comment_rating">
-              评分${rating}
-            </span>
-            <span class="comment_time">
-              ${time}
-            </span>
+          <div class="avatar">
+            <a href="${comment.author.alt}">
+              <img src="${comment.author.avatar}"/>
+            </a>
           </div>
-          <div class="comment_text">
-            ${content}
+          <div class="comment_main">
+            <div class="comment_head">
+              ${comment.author.name}
+              <span class="comment_rating">
+                评分：${comment.rating.value}
+              </span>
+              <span class="comment_time">
+                ${comment.created_at}
+              </span>
+            </div>
+            <div class="comment_text">
+              ${comment.content}
+            </div>
           </div>
         </div>`;
   let comments = document.getElementById('comments');
