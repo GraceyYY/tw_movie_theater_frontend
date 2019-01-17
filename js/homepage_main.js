923b43fec6f3c2aa3d589f032e09457f79392ddd
Grace <lunay0325@gmail.com>
2019-01-18 03:54:28 +0800

    Delete unnecessary code in showPosters function

--- js/homepage_main.js
+++ js/homepage_main.js
@@ -88,7 +88,6 @@ function loadMore() {
 const imgUrls = ['http://puui.qpic.cn/media_img/0/vnewpictag_4_81_1546932831904820_14323_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_6_353_1547193470360262_32385_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_8_1292_1547193478591265_30717_1680_580/0'];
 
 function showPosters() {
-  console.log('test');
   const poster = document.getElementById('big_poster');
   for (let i = 0; i < imgUrls.length; i++) {
     setTimeout(() => {
