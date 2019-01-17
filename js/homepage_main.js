b8916fd131771fbcab5c16d0a56c216853bd636a
Grace <lunay0325@gmail.com>
2019-01-18 01:56:58 +0800

    Complete the function of changing posters on the homepage

--- js/homepage_main.js
+++ js/homepage_main.js
@@ -83,3 +83,15 @@ function showMovies(num, movies, dom) {
 function loadMore() {
   showMovies(20, movies, list);
 }
+const imgUrls = ['http://puui.qpic.cn/media_img/0/vnewpictag_4_81_1546932831904820_14323_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_6_353_1547193470360262_32385_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_8_1292_1547193478591265_30717_1680_580/0'];
+
+function showPosters() {
+  console.log('test');
+  const poster = document.getElementById('big_poster');
+  for (let i = 0; i < imgUrls.length; i++) {
+    setTimeout(() => {
+      poster.setAttribute('src', imgUrls[i]);
+    }, 5000 * i);
+  }
+}
+setInterval('showPosters()', 15000);
