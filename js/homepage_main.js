9673631a65dca77bf2b3f742c85f30085ffb26aa
Grace <lunay0325@gmail.com>
2019-01-23 16:14:53 +0800

    edit the name of some variables

--- js/homepage_main.js
+++ js/homepage_main.js
@@ -8,21 +8,21 @@ Papa.parse("../data/movies.csv", {
     localStorage.setItem('movie', movieData);
   }
 });
-const classification = document.getElementById('classification');
-const data = new MovieData(JSON.parse(localStorage.getItem('movie')));
+const DATA = new MovieData(JSON.parse(localStorage.getItem('movie')));
+let classification = document.getElementById('classification');
 let movieList;
 init();
 classification.addEventListener('click', event => {
   if (event.target.nodeName === 'SPAN') {
     resetPage();
     switchGenre(event.target);
-    movieList.movies = data.searchByGenre([event.target.innerText]);
+    movieList.movies = DATA.searchByGenre([event.target.innerText]);
     movieList.showMovie(20);
   }
 });
 
 function init() {
-  const list = document.getElementsByClassName('movie_list')[0];
+  let list = document.getElementsByClassName('movie_list')[0];
   let errorMessege = document.getElementById('error_messege');
   let loadMore = document.getElementById('load_more');
   movieList = new MovieList(list, loadMore, errorMessege);
@@ -30,7 +30,7 @@ function init() {
     search(localStorage.getItem('search'));
     localStorage.removeItem('search');
   } else {
-    movieList.movies = data.searchByGenre(['剧情']);
+    movieList.movies = DATA.searchByGenre(['剧情']);
     movieList.showMovie(20);
   }
 }
@@ -47,13 +47,13 @@ function searchMovie() {
 
 function search(value) {
   resetPage();
-  if (data.genresToId[value]) {
-    movieList.movies = data.searchByGenre([value]);
+  if (DATA.genresToId[value]) {
+    movieList.movies = DATA.searchByGenre([value]);
   } else {
-    let names = data.nameToId.keys();
+    let names = DATA.nameToId.keys();
     for (let name of names) {
       if (name.includes(value.toLowerCase())) {
-        movieList.movies.push(data.searchByName(name));
+        movieList.movies.push(DATA.searchByName(name));
       }
     }
   }
--- js/homepage_view.js
+++ js/homepage_view.js
@@ -1,3 +1,6 @@
+
+const IMG_URLS = ['http://puui.qpic.cn/media_img/0/vnewpictag_4_81_1546932831904820_14323_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_6_353_1547193470360262_32385_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_8_1292_1547193478591265_30717_1680_580/0'];
+
 function clearChosenSubject() {
   let subjects = document.getElementsByClassName('subject');
   for (let subject of subjects) {
@@ -9,13 +12,11 @@ function switchGenre(genre) {
   genre.classList.add('chosen');
 }
 
-const imgUrls = ['http://puui.qpic.cn/media_img/0/vnewpictag_4_81_1546932831904820_14323_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_6_353_1547193470360262_32385_1680_580/0', 'http://puui.qpic.cn/media_img/0/vnewpictag_8_1292_1547193478591265_30717_1680_580/0'];
-
 function showPosters() {
   const poster = document.getElementById('big_poster');
-  for (let i = 0; i < imgUrls.length; i++) {
+  for (let i = 0; i < IMG_URLS.length; i++) {
     setTimeout(() => {
-      poster.setAttribute('src', imgUrls[i]);
+      poster.setAttribute('src', IMG_URLS[i]);
     }, 5000 * i);
   }
 }
